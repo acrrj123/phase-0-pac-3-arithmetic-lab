b7d072a5b37e2ccbd94abb5a9772ea90b442ac5b
Anna Raptidou Cole <acrrj123@gmail.com>
2024-06-02 17:18:17 -0500

    solution

---
+++ index.js
@@ -0,0 +1,36 @@
+// Put -- bail in the end of the script test in package.json to see one test at a time. Then run npm install and npm test in the terminal. 
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function increment(n) {
+  n++
+  return n
+}
+
+function decrement(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
